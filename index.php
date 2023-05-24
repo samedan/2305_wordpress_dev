64b8abffa98e150b8b607a723fb4166cbbcf63f4
Daniel Popescu <same.dan@gmail.com>
2023-05-24 16:26:13 +0200

    Children links, Menu Links

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
   get_header();
 ?>
 
+<?php 
+  function doubleMe($x){
+    return $x * 2;
+  }
+
+  echo doubleMe(4);
+
+?>
+
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')  ?>)"></div>
       <div class="page-banner__content container t-center c-white">
--- page.php
+++ page.php
@@ -18,22 +18,61 @@
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
+
+       <!-- BREADCRUMB -->
+       <?php 
+          $theParent = wp_get_post_parent_id(get_the_ID());
+          
+          // echo wp_get_post_parent_id(get_the_ID()); 0 if the page IS a PARENT
+          if($theParent) {
+            // echo "CHILD Page";
+            ?>
+              <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                  <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
+                  <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> 
+                  <span class="metabox__main"><?php the_title() ?></span>
+                </p>
+              </div>
+            <?php 
+          }
+          
+       ?>
+      
 
       <!-- Sidebar on the right -->
-      <!-- 
+      <?php 
+          // verify is a Parent page has Children
+          $testArray = get_pages(array(
+            'child_of' => get_the_ID()
+          ));
+          if($theParent or $testArray) {
+      ?>
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent)  ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php 
+              // get the ID if Parent or Child
+              if($theParent) {
+                // CHILD
+                $findChildrenOf = $theParent;
+              } else {
+                // PARENT
+                $findChildrenOf = get_the_ID();
+              }
+              wp_list_pages(array(
+                'title_li' => NULL, // Name of the Parent nor displayed (null)
+                'child_of' => $findChildrenOf,
+                'sort_column' => 'menu_order'
+              ));
+          ?>
+          <!-- <li class="current_page_item"><a href="#">Our History</a></li>
+          <li><a href="#">Our Goals</a></li> -->
         </ul>
       </div>
-  -->
+      <?php } ?>
+      <!-- END Sidebar on the right -->
+  
 
       <div class="generic-content">
        <?php the_content(); ?>
