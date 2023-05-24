b9a86a767967c5dec41ce6527e3b913ae7c75162
Daniel Popescu <same.dan@gmail.com>
2023-05-24 11:44:20 +0200

    Transform html theme to Wordpress

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php 
 
  function university_files() {
+   // load Javascript
+   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true); // array('jquery') = dependencies
    // load styles.css
-   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+
+   // load external
+   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
  }
 
  // load file = wp_enqueue_scripts
