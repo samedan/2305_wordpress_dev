e798b9eb945c07e7768464376f796177c71774e4
Daniel Popescu <same.dan@gmail.com>
2023-06-09 14:31:43 +0200

    Manually added Custom fields (not plugin)

--- university-post-types.php
+++ university-post-types.php
@@ -5,7 +5,7 @@
 function university_post_types() {
   register_post_type('event', array(
     'show_in_rest' => true,
-    'supports' => array('title', 'editor', 'excerpt'),
+    'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
