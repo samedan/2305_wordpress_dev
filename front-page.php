19b4e3be5f2e8e33595815ec3993480218ef724b
Daniel Popescu <same.dan@gmail.com>
2023-06-09 11:15:15 +0200

    Custom Post Type single, Archive

--- front-page.php
+++ front-page.php
@@ -2,14 +2,7 @@
   get_header();
 ?>
 
-<?php 
-  function doubleMe($x){
-    return $x * 2;
-  }
 
-  echo doubleMe(4);
-
-?>
 
   <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')  ?>)"></div>
@@ -21,35 +14,44 @@
       </div>
     </div>
 
+    <!-- EVENTS -->
     <div class="full-width-split group">
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+          
+          <?php
+            
+            $homepageEvents = new WP_Query(array(
+              'posts_per_page'=> 2,
+              'post_type' => 'event', // what data to query from teh DBB
+            ));
 
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-              <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
+            while($homepageEvents->have_posts()) {
+              $homepageEvents->the_post(); ?>
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="#">
+                  <span class="event-summary__month">Mar</span>
+                  <span class="event-summary__day">25</span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title()  ?></a></h5>
+                  <p> <?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+                </div>
+              </div>
+              <?php
+            }
+          ?>
+
+          
+          
 
           <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
+
+
+      <!-- POSTS -->
       <div class="full-width-split__two">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
