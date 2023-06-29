9e88585b738725f0cb1941c7089bc1b08dd404a6
Daniel Popescu <same.dan@gmail.com>
2023-06-29 16:46:46 +0200

    Display search results Title &Link2

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -1,84 +1,95 @@
 import $ from "jquery";
 
 class Search {
+  // 1. describe and create/initiate our object
   constructor() {
     this.resultsDiv = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
-    this.searchField = $("#search-term"); // down in footer
+    this.searchField = $("#search-term");
     this.events();
     this.isOverlayOpen = false;
     this.isSpinnerVisible = false;
-    // keeps track of the previous search value
-    this.typingValue;
+    this.previousValue;
     this.typingTimer;
   }
 
-  // events
+  // 2. events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
     $(document).on("keydown", this.keyPressDispatcher.bind(this));
-    this.searchField.on("keydown", this.typingLogic.bind(this));
+    this.searchField.on("keyup", this.typingLogic.bind(this));
   }
 
-  // methods
+  // 3. methods (function, action...)
+  typingLogic() {
+    if (this.searchField.val() != this.previousValue) {
+      clearTimeout(this.typingTimer);
+
+      if (this.searchField.val()) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      } else {
+        this.resultsDiv.html("");
+        this.isSpinnerVisible = false;
+      }
+    }
+
+    this.previousValue = this.searchField.val();
+  }
+
+  getResults() {
+    $.getJSON(
+      "http://fictional-university.local/wp-json/wp/v2/posts?search=" +
+        this.searchField.val(),
+      (posts) => {
+        this.resultsDiv.html(`
+          <h2 class="search-overlay__section-title">General Information</h2>
+          <ul class="link-list min-list">
+            ${posts
+              .map(
+                (item) =>
+                  `<li><a href="${item.link}">${item.title.rendered}</a></li>`
+              )
+              .join("")}   
+          </ul> 
+        `);
+      }
+      // <li><a href="${posts[0].link}">${posts[0].title.rendered}</a></li>
+    );
+  }
+
+  keyPressDispatcher(e) {
+    if (
+      e.keyCode == 83 &&
+      !this.isOverlayOpen &&
+      !$("input, textarea").is(":focus")
+    ) {
+      this.openOverlay();
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    console.log("our open method just ran!");
     this.isOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
+    console.log("our close method just ran!");
     this.isOverlayOpen = false;
-  }
-
-  typingLogic() {
-    // only if the keystroke changed the search field in some way
-    if (this.searchField.val() != this.previousValue) {
-      clearTimeout(this.typingTimer); // wait 2 seconds before sending input data
-
-      if (this.searchField.val()) {
-        // field has text
-        if (!this.isSpinnerVisible) {
-          // if in the 2 seconds visible delay
-          this.resultsDiv.html('<div class="spinner-loader"></div>');
-          this.isSpinnerVisible = true;
-        }
-        // runs after every key stroke
-        this.previousValue = this.searchField.val();
-      } else {
-        // field is empty, deleted inout
-        this.resultsDiv.html("");
-        this.isSpinnerVisible = false;
-      }
-    }
-    this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
-  }
-
-  getResults() {
-    this.resultsDiv.html("search results");
-    this.isSpinnerVisible = false;
-  }
-
-  keyPressDispatcher(e) {
-    // console.log(e.keyCode);
-    if (
-      e.keyCode == 83 && // s key
-      !this.isOverlayOpen &&
-      !$("input, textarea").is(":focus")
-    ) {
-      this.openOverlay();
-      console.log("ran open");
-    }
-    if (e.keyCode == 27 && this.isOverlayOpen) {
-      // escape key
-      this.closeOverlay();
-      console.log("ran close");
-    }
   }
 }
 
