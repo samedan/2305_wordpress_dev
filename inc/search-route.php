d235366d17ab36e263a75e02a69b106d981f98d3
Daniel Popescu <same.dan@gmail.com>
2023-07-11 11:33:47 +0200

    Finished Search

--- inc/search-route.php
+++ inc/search-route.php
@@ -52,6 +52,17 @@ function universitySearchResults($data) {
        ));
     }
     if(get_post_type() == 'program') {
+      $relatedCampuses = get_field('related_campus'); // related_campus is array
+      if($relatedCampuses) {
+        foreach($relatedCampuses as $campus) {
+            array_push(
+              $results['campuses'], 
+              array(
+                'title' => get_the_title($campus),
+                'permalink' => get_the_permalink($campus)
+              ));
+        }
+      }
       array_push($results['programs'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
@@ -100,7 +111,10 @@ function universitySearchResults($data) {
   
     // Search based on relationships (Professor has Related program Biology)
     $programRelationshipQuery = new WP_Query(array(
-      'post_type' => 'professor',
+      'post_type' => array(
+        'professor', 
+        'event'
+      ),
       'meta_query' => $programsMetaQuery
     ));
   
@@ -108,22 +122,43 @@ function universitySearchResults($data) {
     while($programRelationshipQuery->have_posts()) {
       $programRelationshipQuery->the_post();
   
+      if(get_post_type() == 'event') {
+          $eventDate = new DateTime(get_field('event_date'));
+          $description = null;
+          if(has_excerpt()) {
+            $description = get_the_excerpt();
+          } else {
+            $description = wp_trim_words(get_the_content(), 18);
+          }
+          array_push($results['events'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'month' => $eventDate->format('M'),
+            'day' => $eventDate->format('d'),
+            'description' => $description
+          ));
+      } 
       if(get_post_type() == 'professor') {
-        array_push($results['professors'], array(
-          'title' => get_the_title(),
-          'permalink' => get_the_permalink(),
-          'image' => get_the_post_thumbnail_url(
-            0, // current post
-            'professorLandscape' // size
-          ),
-         ));
+          array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(
+              0, // current post
+              'professorLandscape' // image size
+            ),
+          ));
       }
+      
     }
   
     // remove duplicates
     $results['professors'] =
     array_values( // removes the "id0=" from "0": { "title": "Dr. Barksalot"...
      array_unique($results['professors'], SORT_REGULAR)
+    );
+    $results['events'] =
+    array_values( // removes the "id0=" from "0": { "title": "Dr. Barksalot"...
+     array_unique($results['events'], SORT_REGULAR)
     );
   }
 
