c235a33fc88798bf731b9ec8d1e700296360c382
Daniel Popescu <same.dan@gmail.com>
2023-07-19 10:29:30 +0200

    Sanitize post content

--- functions.php
+++ functions.php
@@ -99,7 +99,12 @@ function pageBanner($args = NULL) { // $args is optional
 function makeNotePrivate($data) {
     if($data['post_type'] == 'note' AND $data['post_status'] != 'trash'){
       $data['post_status'] = "private";
+      $data['post_title'] = sanitize_text_field($data['post_title']);
     } 
+    // Sanitize post content
+    if($data['post_type'] == 'note'){
+      $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    }
     return $data;
 }
 
--- page-my-notes.php
+++ page-my-notes.php
@@ -32,11 +32,17 @@
              while($userNotes->have_posts()) {
                 $userNotes->the_post(); ?>
                 <li data-id="<?php the_ID(); ?>">
-                  <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+                <!-- replace private -->  
+                <input readonly class="note-title-field" 
+                      value="<?php echo str_replace(
+                      'Private: ',
+                      '',
+                      esc_attr(get_the_title())
+                      ) ?>">
                   <span class="edit-note"><i class="fa fa-pencil" area-hidden="true"></i> Edit</span>
                   <span class="delete-note"><i class="fa fa-trash-o" area-hidden="true"></i> Delete</span>
                   <textarea readonly class="note-body-field">
-                    <?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?>
+                    <?php echo esc_textarea(get_the_content()); ?>
                   </textarea>
 
                   <span class="update-note btn btn--blue btn--small">
